48f944a2d0f08bb691b89ef4bbe3b9b21e0fdd67
SaltenoA <arielms.9472@gmail.com>
2026-02-19 11:41:10 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -45,3 +45,99 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
 });
+
+const slides = document.querySelectorAll(".slide");
+const thumbTrack = document.querySelector(".thumb-track");
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+const thumbPrev = document.querySelector(".thumb-prev");
+const thumbNext = document.querySelector(".thumb-next");
+
+let index = 0;
+let interval;
+
+// Crear miniaturas dinámicamente
+slides.forEach((slide, i) => {
+  const img = slide.querySelector("img").cloneNode();
+  if(i === 0) img.classList.add("active");
+
+  img.addEventListener("click", () => showSlide(i));
+  thumbTrack.appendChild(img);
+});
+
+const thumbs = document.querySelectorAll(".thumb-track img");
+
+function showSlide(i) {
+  slides[index].classList.remove("active");
+  thumbs[index].classList.remove("active");
+
+  index = i;
+
+  if(index >= slides.length) index = 0;
+  if(index < 0) index = slides.length - 1;
+
+  slides[index].classList.add("active");
+  thumbs[index].classList.add("active");
+}
+
+function nextSlide() {
+  showSlide(index + 1);
+}
+
+function prevSlide() {
+  showSlide(index - 1);
+}
+
+next.addEventListener("click", nextSlide);
+prev.addEventListener("click", prevSlide);
+
+// Auto play
+function startAuto() {
+  interval = setInterval(nextSlide, 5000);
+}
+
+function stopAuto() {
+  clearInterval(interval);
+}
+
+document.querySelector(".slider").addEventListener("mouseenter", stopAuto);
+document.querySelector(".slider").addEventListener("mouseleave", startAuto);
+
+startAuto();
+
+let thumbPosition = 0;
+
+const thumbContainer = document.querySelector(".thumb-container");
+
+function updateThumbLimits() {
+  const maxScroll = thumbTrack.scrollWidth - thumbContainer.clientWidth;
+  return maxScroll > 0 ? maxScroll : 0;
+}
+
+thumbNext.addEventListener("click", () => {
+  const maxScroll = updateThumbLimits();
+
+  if (Math.abs(thumbPosition) < maxScroll) {
+    thumbPosition -= thumbContainer.clientWidth / 2;
+
+    if (Math.abs(thumbPosition) > maxScroll) {
+      thumbPosition = -maxScroll;
+    }
+
+    thumbTrack.style.transform = `translateX(${thumbPosition}px)`;
+  }
+});
+
+thumbPrev.addEventListener("click", () => {
+  if (thumbPosition < 0) {
+    thumbPosition += thumbContainer.clientWidth / 2;
+
+    if (thumbPosition > 0) {
+      thumbPosition = 0;
+    }
+
+    thumbTrack.style.transform = `translateX(${thumbPosition}px)`;
+  }
+});
+
+
